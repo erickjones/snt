9b908d1dc8d81a7b0dbeab56ebc16576e80dd232
Erick Jones <erick.jones@gmail.com>
2016-08-21 00:12:01 +0200

    More small improvements

--- app/follow/follow-controller.js
+++ app/follow/follow-controller.js
@@ -3,16 +3,18 @@
 	.controller('FollowController', ['$scope', '$http', function($scope, $http){
 
 		$scope.user = JSON.parse(localStorage['User-Data']);
-		console.log($scope.user);
+		//console.log($scope.user);
 	  $http.get('api/users/get').then(function(response){
 			$scope.users = response.data;
-			console.log($scope.users);
+			//console.log($scope.users);
 		})
 
 		$scope.follow = function(userId, wasterId) {
 			request = { userId: userId, wasterId: wasterId};
 			$http.post('api/users/follow', request).then(function(){
 				console.log("following ", wasterId);
+				localStorage.setItem('User-Data', JSON.stringify(response));
+				$state.reload();
 			})
 		}
 
@@ -23,6 +25,9 @@
 				}
 			}
 			return false;
+
+			localStorage.setItem('User-Data', JSON.stringify(response));
+			$state.reload();
 		}
 	}]);
 }());
