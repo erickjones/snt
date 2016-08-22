8c20df2b0555bc9fa03dde98cee9e62f22196f15
Erick Jones <erick.jones@gmail.com>
2016-08-22 08:12:21 +0200

    Reindenting code. Nothing new.

--- app/app.js
+++ app/app.js
@@ -1,34 +1,34 @@
 (function(){
-    angular.module('snt', ['ui.router', 'ngFileUpload'])
-            .config(function($stateProvider, $urlRouterProvider){
+  angular.module('snt', ['ui.router', 'ngFileUpload'])
+  .config(function($stateProvider, $urlRouterProvider){
 
-            $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
-            $stateProvider
-                .state('signUp', {
-                url: "/signup",
-                templateUrl: "app/signup/signup.html",
-                controller: "SignupController"
-            })
-                .state('editProfile', {
-                url: "/edit-profile",
-                templateUrl: "app/profile/edit-profile-view.html",
-                controller: "EditProfileController"
-            })
-              .state('main', {
-                url: "/",
-                templateUrl: "app/main/main.html",
-                controller: "MainController"
-            })
-              .state('profile', {
-                url: "/profile",
-                templateUrl: "app/profile/profile-view.html",
-                controller: "EditProfileController"
-            })
-      			  .state('follow', {
-      				url: "/follow-users",
-      				templateUrl: "app/follow/follow.html",
-      				controller: "FollowController"
-      			})
-        })
+    $stateProvider
+    .state('signUp', {
+      url: "/signup",
+      templateUrl: "app/signup/signup.html",
+      controller: "SignupController"
+    })
+    .state('editProfile', {
+      url: "/edit-profile",
+      templateUrl: "app/profile/edit-profile-view.html",
+      controller: "EditProfileController"
+    })
+    .state('main', {
+      url: "/",
+      templateUrl: "app/main/main.html",
+      controller: "MainController"
+    })
+    .state('profile', {
+      url: "/profile",
+      templateUrl: "app/profile/profile-view.html",
+      controller: "EditProfileController"
+    })
+    .state('follow', {
+      url: "/follow-users",
+      templateUrl: "app/follow/follow.html",
+      controller: "FollowController"
+    })
+  })
 }());
